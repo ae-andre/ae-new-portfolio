8ac504a53e7b385438d64f22ee7ff8b95a05f320
ae-andre <aendique@gmail.com>
2024-03-20 13:37:56 -0600

    created about page

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
-
-function About(props) {
-    return (
-        <div className="main">
-            <h1>About Page</h1>
+export default function About() {
+  return (
+    <div className="bg-white">
+      <main className="isolate">
+        {/* Hero section */}
+        <div className="relative isolate -z-10 overflow-hidden bg-gradient-to-b from-indigo-100/20 pt-14">
+          <div
+            className="absolute inset-y-0 right-1/2 -z-10 -mr-96 w-[200%] origin-top-right skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:-mr-80 lg:-mr-96"
+            aria-hidden="true"
+          />
+          <div className="mx-auto max-w-7xl px-6 py-32 sm:py-40 lg:px-8">
+            <div className="mx-auto max-w-2xl lg:mx-0 lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-x-16 lg:gap-y-6 xl:grid-cols-1 xl:grid-rows-1 xl:gap-x-8">
+              <h1 className="max-w-2xl text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl lg:col-span-2 xl:col-auto">
+                I'm passionate about building bridges through code and a curious
+                mindset.
+              </h1>
+              <div className="mt-6 max-w-xl lg:mt-0 xl:col-end-1 xl:row-start-1">
+                <p className="text-lg leading-8 text-gray-600">
+                  I'm Andre Endique, a multifaceted Developer. Recently
+                  relocated from Vancouver to Edmonton with my wonderful
+                  family—my constant inspiration. I dabble in portrait
+                  photography and I enjoy dancing and fitness.
+                </p>
+              </div>
+              <img
+                src="..\src\assets\about me image.jpg"
+                alt="photo of man outdoors at sunset"
+                className="mt-10 aspect-[6/5] w-full max-w-lg rounded-2xl object-cover sm:mt-16 lg:mt-0 lg:max-w-none xl:row-span-2 xl:row-end-2 xl:mt-36"
+              />
+            </div>
+          </div>
         </div>
-    );
+      </main>
+    </div>
+  );
 }
-
-export default About;
