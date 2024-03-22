93ad17bbca68fb367147b4e07af2c28c393a0b4d
ae-andre <aendique@gmail.com>
2024-03-22 17:12:19 -0600

    fixed vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  root: 'src',
+  publicDir: 'public',
+  outDir: '../docs',
+  server: {
+    open: true
+  },
   plugins: [react()],
 })
