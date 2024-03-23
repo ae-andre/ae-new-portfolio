41c286127b5790b5ba4df29b1e925aeb4cbf1599
ae-andre <aendique@gmail.com>
2024-03-22 21:06:45 -0600

    changed paths in main

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Portfolio from './pages/Portfolio/index.jsx'
-import About from './pages/About/index.jsx'
-import Contact from './pages/Contact/index.jsx'
-import Resume from './pages/Resume/index.jsx'
+import Portfolio from './pages/Portfolio'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Resume from './pages/Resume'
 
 
 const router = createBrowserRouter([
@@ -19,15 +19,15 @@ const router = createBrowserRouter([
         element: <About/>
       },
       {
-        path: "portfolio",
+        path: "/portfolio",
         element: <Portfolio/>
       },
       {
-        path: "contact",
+        path: "/contact",
         element: <Contact/>
       },
       {
-        path: "resume",
+        path: "/resume",
         element: <Resume/>
       },
     ]
